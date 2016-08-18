6a3bf1851e2f311652515dfb8d23411ada789238
Georgina Van Dort and Stephanie Gurung <georgina@>
2016-08-18 14:19:33 -0700

    allow user to delete all comments along with a post

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,12 @@ export default Ember.Route.extend({
   },
   actions: {
     destroyPost(post) {
-      post.destroyRecord();
+      var comment_deletions = post.get('comments').map(function(comment){
+        return comment.destroyRecord();
+      });
+      Ember.RSVP.all(comment_deletions).then(function(){
+        return post.destroyRecord();
+      });
       this.transitionTo('index');
     }
   }
