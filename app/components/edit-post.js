7743c5a18047f771368dfb85917672a3ba817d9c
Stephanie Gurung <stephr3@gmail.com>
2016-08-17 18:33:37 -0700

    allow user to edit a post

--- app/components/edit-post.js
+++ app/components/edit-post.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    edit(post){
+      var params = {
+        title: this.get('title'),
+        date: this.get('date'),
+        neighborhood: this.get('neighborhood'),
+        address: this.get('address'),
+        price: this.get('price'),
+        rating: this.get('rating'),
+        imgurl: this.get('imgurl'),
+        text: this.get('text')
+      };
+      this.sendAction('edit', post, params);
+    }
+  }
 });
--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    edit(post, params) {
+      Object.keys(params).forEach(function(key){
+        if(params[key] !== undefined) {
+          post.set(key, params[key]);
+        }
+      });
+      post.save();
+      this.transitionTo('index');
+    }
+  }    
 });
